3f04c1b0c16adc231ebcc9168b8b85ff4654d3e3
Yana Mironyak <yanika.mironyak@gmail.com>
2022-07-24 22:44:00 +0300

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import css from './App.module.css';
+
+
+import ContactsForm from "./ContactsForm/ContactsForm";
+import ContactsList from "components/ContactsList/ContactsList";
+import Filter from 'components/Filter/Filter';
+import { nanoid } from "nanoid";
+
+export class App extends React.Component {
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  }
+
+  handleSubmit = ({ name, number }, { resetForm }) => {
+    const id = nanoid();
+    this.state.contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase()) ? alert(`${name} is already in contacts`) :
+    this.setState(prevState => ({contacts: [...prevState.contacts, {name, number, id}]}))
+    resetForm();
+  };
+
+  changeFilter = (evt) => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()));
+  };
+
+  deleteContact = ( contactId ) => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId)
+    }))
+  }
+  
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+    const { name, number } = this.state;
+    
+    return (
+      <div className={css.container}>
+        <h1 className={css.mainTitle}>Phonebook</h1>
+        <ContactsForm initialValues={{name, number}} onSubmit={this.handleSubmit}/>
+        <h2 className={css.title}>Contacts</h2>
+        <Filter filter={this.state.filter} changeFilter={this.changeFilter} />
+        <ContactsList filteredContacts={filteredContacts} onDeleteContact={this.deleteContact} />
+      </div>
+    )
+  }
+}
